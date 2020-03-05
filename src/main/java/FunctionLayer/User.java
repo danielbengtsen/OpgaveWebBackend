c50e73f528d82e5c153c33d00cc578d4a6d186d1
Bloom9900 <jannichhoejmose@gmail.com>
2020-03-05 11:06:47 +0100

    ToString i User.java tilpasset manuelt

--- src/main/java/FunctionLayer/User.java
+++ src/main/java/FunctionLayer/User.java
@@ -51,7 +51,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "User: " + "email='" + email + '\'' + ", password='" + password + '\'' +
-                ", role='" + role;
+        return "User: email=" + email + ", password=" + password + ", role=" + role;
     }
 }
