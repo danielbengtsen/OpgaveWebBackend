e177f2eecf6dffbfecd7e3c47a0ede51293e2dc6
Bloom9900 <jannichhoejmose@gmail.com>
2020-03-05 13:11:41 +0100

    Nu tror jeg vi er helt færdige, uden at have testet igen

--- src/main/java/PresentationLayer/ChangePassword.java
+++ src/main/java/PresentationLayer/ChangePassword.java
@@ -11,12 +11,19 @@ public class ChangePassword extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
         String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        UserMapper.changePassword(email, password);
+        String password1 = request.getParameter("password1");
+        String password2 = request.getParameter("password2");
 
-        HttpSession session = request.getSession();
+        if(password1.equals(password2)) {
+            UserMapper.changePassword(email, password1);
+            HttpSession session = request.getSession();
 
-        session.setAttribute("besked", "Brugeren med Email: "+ email +" har fået ændret password.");
-        return "employeepage";
+            session.setAttribute("besked", "Brugeren med Email: "+ email +" har fået ændret password.");
+            return "employeepage";
+        } else {
+            throw new LoginSampleException("The two passwords did not match");
+        }
+
+
     }
 }
